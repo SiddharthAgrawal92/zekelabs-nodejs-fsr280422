136656f58be881b3930a71ab629fa30369e03b2e
SiddharthAgrawal92 <47270428+SiddharthAgrawal92@users.noreply.github.com>
2022-11-08 21:33:43 +0530

    Day 15 - Data Validation & Sanitization

--- Sessions/Express App/src/api-routes/players.routes.js
+++ Sessions/Express App/src/api-routes/players.routes.js
@@ -1,10 +1,11 @@
 const playersRoutes = require('express').Router();
 const playersControllers = require('../controllers/players.controllers');
+const playersValidators = require('../validators/players.validators');
 
 //create players
-playersRoutes.post('/', playersControllers.createPlayers);
+playersRoutes.post('/', playersValidators.validateCreatePlayers, playersControllers.createPlayers);
 
 //get players
-playersRoutes.get('/', playersControllers.getPlayers);
+playersRoutes.get('/', playersValidators.validateGetPlayers, playersControllers.getPlayers);
 
 module.exports = playersRoutes;
---
+++ Sessions/Express App/src/validators/players.validators.js
@@ -0,0 +1,77 @@
+const { check, validationResult, query } = require('express-validator');
+
+const validateCreatePlayers = async (req, res, next) => {
+    //## Validations
+    //check if string
+    // await check('name', 'Name should be a string').isString().run(req);
+
+    // //check for length
+    // await check('name', 'Length is not in the range').isLength({ min: 6, max: 30 }).run(req);
+
+    //chaining of validations & bail() to stop the next validation to be called in the chain
+    await check('name', 'Name should be a string & in a valid range').isString().bail().isLength({ min: 6, max: 30 }).run(req);
+
+    //required field
+    // await check('manager', 'Field is required').exists().run(req);
+
+    //check if should be empty
+    // await check('manager', 'Field should be empty').isEmpty().run(req);
+
+    //validate email
+    // await check('username', 'Please send a correct email').isEmail().run(req);
+
+    // //validate alphanumeric password with length
+    // // await check('password', 'Please send a password in correct format').isAlphanumeric().isLength({ min: 7, max: 16 }).run(req);
+
+    // //validate strong password with length
+    // await check('password', 'Please send a password in correct format').isStrongPassword({ minLength: 7, minLowercase: 1, minUppercase: 1, minSymbols: 1 }).isLength({ max: 16 }).run(req);
+
+    // //to validate the value of confirm password
+    // await check('confirmPassword', 'confirmPassword & password should be same').custom((value, { req }) => value === req.body.password).run(req);
+
+    // //optional to check but if passed then check with specified values
+    // await check('version', 'version should be correct').optional().isIn(["1.0.0", "1.0.1"]).run(req);
+
+    // //validate a correct UUID --> 8-4-4-4-12
+    // await check('uuid', 'uuid should be correct').isUUID(["1.0.0", "1.0.1"]).run(req);
+
+    // //## Sanitization
+    // //replace values with new one 
+    // //[foo, bar]  has to be replace with --> football
+    // await check('note').replace(["foo", "bar"], "football").trim().run(req);
+
+    // //set value to UpperCase
+    // await check('note').toUpperCase().run(req);
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        next();
+    } else {
+        res.status(400).send({ errors: errors.array() });
+    }
+
+}
+
+const validateGetPlayers = async (req, res, next) => {
+
+    await query('skip', 'skip value is invalid').isNumeric().custom((value) => value >= 0).run(req);
+    await query('limit', 'limit value is invalid').isNumeric().custom((value) => value >= 1).run(req);
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        next();
+    } else {
+        res.status(400).send({ errors: errors.array() });
+    }
+}
+
+
+module.exports = {
+    validateCreatePlayers,
+    validateGetPlayers
+}
+
+//API - DOCUMENT
+//1. Endpoint --> http://localhost:8080/players
+//2. Method --> POST
+//3. Request --> { name: <String> <Length {Min:6, Max: 30} >}
